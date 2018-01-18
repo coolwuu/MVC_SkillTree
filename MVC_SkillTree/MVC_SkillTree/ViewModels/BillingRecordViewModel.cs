eca3c15ea6eedf0c00f56f14142cc901c7df60d6
coolwuu <coolwuu0207@gmail.com>
2018-01-18 21:20:52 +0800

    add BillingRecordViewModel and BillingType Enum

---
+++ MVC_SkillTree/MVC_SkillTree/ViewModels/BillingRecordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SkillTree.ViewModels
+{
+    public class BillingRecordViewModel
+    {
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Remark { get; set; }
+
+        public BillingType Type { get; set; }
+    }
+
+    public enum BillingType
+    {
+        Revenue,
+        Expenses
+    }
+}
