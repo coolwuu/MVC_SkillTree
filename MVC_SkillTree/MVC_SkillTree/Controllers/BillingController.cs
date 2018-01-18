f2295d47eed7da167e60d0189592ccbc04de064d
coolwuu <coolwuu0207@gmail.com>
2018-01-18 16:02:24 +0800

    Add BillingController. Add List Method

---
+++ MVC_SkillTree/MVC_SkillTree/Controllers/BillingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVC_SkillTree.Controllers
+{
+    public class BillingController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
