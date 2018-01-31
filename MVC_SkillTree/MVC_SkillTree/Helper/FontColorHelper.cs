fb73945364595d03d8dfa7873f253423dcef79ee
coolwuu <coolwuu0207@gmail.com>
2018-01-31 23:59:46 +0800

    add html helper to add font color style by Type.

---
+++ MVC_SkillTree/MVC_SkillTree/Helper/FontColorHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_SkillTree.Models;
+using MVC_SkillTree.ViewModels;
+
+namespace MVC_SkillTree.Helper
+{
+    public static class FontColorHelper
+    {
+        public static MvcHtmlString AddColorByCategory(this HtmlHelper htmlHelper, BillingRecordViewModel record)
+        {
+            return record.Type == BillingType.支出 ? new MvcHtmlString($"<span style=\"color:red;\">{record.Type}</span>")
+                : new MvcHtmlString($"<span style=\"color:blue;\">{record.Type}</span>");
+        }
+    }
+}
