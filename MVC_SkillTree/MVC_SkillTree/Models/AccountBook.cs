58e6319ae488dbf4c91d3c03feff61d9578a1280
coolwuu <coolwuu0207@gmail.com>
2018-02-02 09:45:23 +0800

    整理AccountBook attribute

--- MVC_SkillTree/MVC_SkillTree/Models/AccountBook.cs
+++ MVC_SkillTree/MVC_SkillTree/Models/AccountBook.cs
@@ -12,19 +12,23 @@ namespace MVC_SkillTree.Models
     {
         public Guid Id { get; set; }
 
-        [Required(ErrorMessage = "此欄位為必填欄位"), Display(Name = "類別")]
+        [Required(ErrorMessage = "此欄位為必填欄位")]
+        [Display(Name = "類別")]
         public BillingType Category { get; set; }
 
-        [Required(ErrorMessage = "此欄位為必填欄位"), Display(Name = "金額")]
-        [Range(0,Double.MaxValue,ErrorMessage = "不得输入负值")]
+        [Required(ErrorMessage = "此欄位為必填欄位")]
+        [Display(Name = "金額")]
+        [Range(0, Double.MaxValue, ErrorMessage = "不得输入负值")]
         public decimal Amount { get; set; }
 
-        [Required(ErrorMessage = "此欄位為必填欄位"), Display(Name = "日期")]
+        [Required(ErrorMessage = "此欄位為必填欄位")]
+        [Display(Name = "日期")]
         [IsExistDateTime(ErrorMessage = "输入的日期不得大于今天")]
         public DateTime Date { get; set; }
 
-        [Required(ErrorMessage = "此欄位為必填欄位"), Display(Name = "備注")]
-        [StringLength(100,ErrorMessage = "備注最大字數為100")]
+        [Required(ErrorMessage = "此欄位為必填欄位")]
+        [Display(Name = "備注")]
+        [StringLength(100, ErrorMessage = "備注最大字數為100")]
         public string Remark { get; set; }
     }
 }
